9b60ecfb83f913e653635afcd8d3592c45cdf31a
GROMIR55 <138116352+GROMIR55@users.noreply.github.com>
2025-01-02 20:34:39 +0400

    Add files via upload

---
+++ Quiz.js
@@ -0,0 +1,110 @@
+const timerElement = document.getElementById("timer");
+const questionElement = document.getElementById("question");
+const answersElement = document.querySelector(".answers");
+const questionsElement = document.getElementById("questions");
+const questionNodes = questionsElement.querySelectorAll(".question-data");
+const scoreElement = document.getElementById("score");
+const endPicElement = document.getElementById("endPic");
+
+let currentQuestionIndex = 0;
+let timeLeft = 10;
+let timer;
+let score = 0;
+
+
+const questions = Array.from(questionNodes).map((questionNode) => ({
+    question: questionNode.dataset.question,
+    image: questionNode.dataset.image,
+    answers: Array.from(questionNode.children).map((answerNode) => ({
+        text: answerNode.dataset.answer,
+        correct: answerNode.dataset.correct === "true",
+    })),
+}));
+
+
+function playSound(soundFile) {
+    var audioPlayer = document.getElementById("sound-player");
+    audioPlayer.src = soundFile;
+    audioPlayer.play();
+}
+
+const questionImageElement = document.getElementById("question-image");
+
+function loadQuestion() {
+    if (currentQuestionIndex >= questions.length) {
+        endQuiz();
+        return;
+    }
+
+    const questionData = questions[currentQuestionIndex];
+    questionElement.textContent = questionData.question;
+
+    questionImageElement.src = questionData.image;
+    questionImageElement.alt = `Изображение для вопроса ${currentQuestionIndex + 1}`;
+
+    answersElement.innerHTML = "";
+
+    questionData.answers.forEach((answer) => {
+        const button = document.createElement("button");
+        button.textContent = answer.text;
+        button.className = "answer-button";
+        button.onclick = () => handleAnswer(answer.correct, button);
+        answersElement.appendChild(button);
+    });
+
+    resetTimer();
+}
+
+
+function resetTimer() {
+    clearInterval(timer);
+    timeLeft = 10;
+    timerElement.textContent = `Осталось времени: ${timeLeft}`;
+    timer = setInterval(() => {
+        timeLeft -= 1;
+        timerElement.textContent = `Осталось времени: ${timeLeft}`;
+        if (timeLeft <= 0) {
+            clearInterval(timer);
+            handleAnswer(false);
+        }
+    }, 1000);
+}
+
+function handleAnswer(isCorrect, button = null) {
+    clearInterval(timer);
+
+    if (button) {
+        if (isCorrect) {
+            button.classList.add("correct");
+            button.style.color = "white";
+            score += 10;
+            playSound('/Sounds/polskaya-korova.mp3');
+        } else {
+            button.classList.add("incorrect");
+            button.style.color = "white";
+            playSound('/Sounds/puk.mp3');
+        }
+    } else {
+        playSound('/Sounds/puk.mp3');
+    }
+
+    // Обновляем счёт
+    scoreElement.textContent = `Счёт: ${score}`;
+
+    setTimeout(() => {
+        currentQuestionIndex++;
+        loadQuestion();
+    }, 1000);
+}
+
+
+function endQuiz() {
+    questionElement.textContent = "Квиз завершён!";
+    answersElement.innerHTML = "";
+    timerElement.textContent = "";
+    questionImageElement.src = endPicElement.textContent;
+    scoreElement.textContent = `Ваш финальный счёт: ${score}`;
+    playSound('/Sounds/polskaya-korova.mp3');
+}
+
+loadQuestion();
---
+++ index.js
@@ -0,0 +1,117 @@
+document.addEventListener("DOMContentLoaded", () => {
+
+    const fullscreenButton = document.getElementById("fullscreen-icon");
+    const iframe = document.querySelector("iframe");
+
+    fullscreenButton.addEventListener("click", () => {
+        if (iframe.requestFullscreen) {
+            iframe.requestFullscreen();
+        } else if (iframe.mozRequestFullScreen) {
+            iframe.mozRequestFullScreen();
+        } else if (iframe.webkitRequestFullscreen) {
+            iframe.webkitRequestFullscreen();
+        } else if (iframe.msRequestFullscreen) {
+            iframe.msRequestFullscreen();
+        } else {
+            console.error("Используй нормальный браузер, советую хром или красную лису.");
+        }
+    });
+
+    const themeSwitch = document.getElementById("theme-switch");
+    const themeIcon = document.getElementById("theme-icon");
+
+    // Установка сохранённой темы
+    const savedTheme = localStorage.getItem("theme") || "dark";
+    document.documentElement.setAttribute("data-theme", savedTheme);
+    themeSwitch.checked = savedTheme === "white";
+    themeIcon.textContent = savedTheme === "white" ? "☀️" : "🌚";
+
+    // Обработчик переключения темы
+    themeSwitch.addEventListener("change", () => {
+        const newTheme = themeSwitch.checked ? "white" : "dark";
+        document.documentElement.setAttribute("data-theme", newTheme);
+        localStorage.setItem("theme", newTheme);
+        themeIcon.textContent = newTheme === "white" ? "☀️" : "🌚";
+    });
+
+    const translations = {
+        en: {
+            home: "Home",
+            design: "Design",
+            memes: "Memes",
+            games: "Games",
+            quizzes: "Quizzes",
+            contact: "Contact",
+            customBigText: "<br>Become the richest businessman in<br>Thunder Coin",
+            customSmallText: "<br><br>Go from poor to rich businessman!<br>Buy upgrades, complete tasks, invite friends and become the best!<br><br>",
+            customButton: "Play!",
+            telegram: "Telegram",
+            twitter: "Twitter",
+            youtube: "YouTube",
+            copyright: "Copyright © 2025 ThunderCoin | All rights reserved.",
+            customFormText: "Technical support"
+        },
+        ru: {
+            home: "Главная",
+            design: "Дизайн",
+            memes: "Мемы",
+            games: "Игры",
+            quizzes: "Викторины",
+            contact: "Контакты",
+            customBigText: "<br>Станьте самым богатым бизнесменом в<br>Thunder Coin",
+            customSmallText: "<br><br>Пройдите путь от бедного бизнесмена до богатого!<br>Покупайте улучшения, выполняйте задания, приглашайте друзей и становитесь лучшим!<br><br>",
+            customButton: "Играть!",
+            telegram: "Телеграм",
+            twitter: "Твиттер",
+            youtube: "Ютуб",
+            copyright: "Авторские права © 2025 ThunderCoin | Все права защищены.",
+            customFormText: "Техническая поддержка"
+        },
+    };
+
+    const savedLanguage = localStorage.getItem("language") || "en";
+    document.documentElement.lang = savedLanguage;
+
+    document.getElementById("language-switch").value = savedLanguage;
+
+    translate(savedLanguage);
+
+    document.getElementById("language-switch").addEventListener("change", (event) => {
+        const selectedLanguage = event.target.value;
+        localStorage.setItem("language", selectedLanguage);
+        document.documentElement.lang = selectedLanguage;
+        translate(selectedLanguage);
+    });
+
+    function translate(language) {
+        document.querySelectorAll("[data-translate]").forEach((element) => {
+            const key = element.getAttribute("data-translate");
+            if (translations[language][key]) {
+                element.innerHTML = translations[language][key];
+            }
+        });
+    }
+
+
+    const scrollTop = document.getElementById("scrollToTopBtn");
+
+    // Показать кнопку при прокрутке вниз
+    window.addEventListener("scroll", () => {
+        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+            scrollTop.style.display = "block";
+            scrollTop.style.opacity = 1;
+        } else {
+            scrollTop.style.opacity = 0;
+            setTimeout(() => {
+                scrollTop.style.display = "none";
+            }, 300);
+        }
+    });
+
+    scrollTop.addEventListener("click", () => {
+        window.scrollTo({
+            top: 0,
+            behavior: "smooth"
+        });
+    });
+});
